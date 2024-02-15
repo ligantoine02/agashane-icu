8534822e899db4b0ed7d00cd238771131e27e8bd
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-02-15 21:58:21 +0200

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ export default {
 			fontFamily: {
 				mono: ["IBM Plex Mono", "ui-monospace"],
 				sans: [
+					"Recursive Variable",
 					"Figtree Variable",
 					"Archivo Variable",
-					"Recursive Variable",
 					"sans-serif",
 				],
 			},
