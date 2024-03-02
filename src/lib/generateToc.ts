aef995993ad6cce39326b6fc6b60f6cfbc94dd76
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-03-02 02:33:15 +0200

    fix: update github token

--- src/lib/generateToc.ts
+++ src/lib/generateToc.ts
@@ -4,19 +4,18 @@ import { fromMarkdown } from "mdast-util-from-markdown";
 import slugify from "slugify";
 
 export const generateToc = (content: string) => {
-	const tree = fromMarkdown(content);
-	const headings = tree.children.filter((child) => child.type === "heading");
-
-	const heads: Array<{ depth: number; slug: string; name: string }> = [];
-	for (const heading of headings) {
-		heads.push({
-			depth: heading.depth,
-			slug: slugify(heading.children[0].value, {
-				lower: true,
-			}),
-			name: heading.children[0].value,
-		});
-	}
-
-	return heads;
+	// const tree = fromMarkdown(content);
+	// const headings = tree.children.filter((child) => child.type === "heading");
+	// const heads: Array<{ depth: number; slug: string; name: string }> = [];
+	// for (const heading of headings) {
+	// 	heads.push({
+	// 		depth: heading.depth,
+	// 		slug: slugify(heading.children[0].value, {
+	// 			lower: true,
+	// 		}),
+	// 		name: heading.children[0].value,
+	// 	});
+	// }
+	// return heads;
+	return [];
 };
