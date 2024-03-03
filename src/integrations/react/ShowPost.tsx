f3b5b0882f2af528e3098aaac91d8e5d64882ce3
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-03-03 20:37:41 +0200

    fix: setup public url

--- src/integrations/react/ShowPost.tsx
+++ src/integrations/react/ShowPost.tsx
@@ -1,8 +1,6 @@
 /** @jsxImportSource react */
 import { qwikify$ } from "@builder.io/qwik-react";
 import Markdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
-import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
 interface Props {
@@ -16,8 +14,7 @@ const ShowPost: React.FC<Props> = ({ content }) => {
 	return (
 		<Markdown
 			remarkPlugins={[remarkGfm]}
-			rehypePlugins={[rehypeHighlight, rehypeSlug]}
-			className="prose prose-slate prose-xl md:prose-2xl dark:prose-invert max-w-none prose-pre:p-0 prose-pre:rounded-lg prose-img:rounded-lg prose-headings:font-darkerGrotesque prose-h3:text-3xl prose-h2:text-4xl prose-a:link prose-a:link-primary prose-a:no-underline prose-lg"
+			className="prose prose-slate prose-xl md:prose-2xl dark:prose-invert max-w-none prose-pre:p-0 prose-pre:rounded-lg prose-img:rounded-lg prose-headings:font-darkerGrotesque prose-h3:text-3xl prose-h2:text-4xl prose-a:link prose-a:link-primary prose-a:no-underline"
 		>
 			{content}
 		</Markdown>
